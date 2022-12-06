351cda603129e7fba8607f24aa61bea063a1256f
Antony Courtney <antony.courtney@gmail.com>
2022-12-06 14:56:31 -0800

    Add rill data httpfs example

--- src/basics.ts
+++ src/basics.ts
@@ -6,4 +6,34 @@ async function simpleTest() {
   console.log(rows);
 }
 
-simpleTest();
+async function rillExample() {
+  console.log("rill data S3 example: ");
+
+  const db = await Database.create(":memory:");
+  const con = await db.connect();
+  await con.exec(`
+    install httpfs;
+    load httpfs;
+    SET s3_endpoint = 'storage.googleapis.com';
+    `);
+  console.log("https extension loaded, fetching data:");
+  const res = await con.all(
+    `SELECT * from 's3://pkg.rilldata.com/example-data/sf311.parquet' LIMIT 5`
+  );
+  console.log(`fetched ${res.length} rows successfully`);
+
+  const s = con.stream(
+    `SELECT * from 's3://pkg.rilldata.com/example-data/sf311.parquet' LIMIT 5`
+  );
+
+  for await (let row of s) {
+    console.log(row);
+  }
+}
+
+async function main() {
+  await simpleTest();
+  await rillExample();
+}
+
+main();
